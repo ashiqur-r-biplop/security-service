af92c611703b9d71f0db7e0e3d0c3f3b5eb03254
ashiqur-r-biplop <biplops22@gmail.com>
2024-05-11 01:05:26 +0600

    Added company image and company secton

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-// nav bar fixed
-window.onscroll = function () {
-  scrollFunction();
-};
+// // nav bar fixed
+// window.onscroll = function () {
+//   scrollFunction();
+// };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").classList.add("navbar-shadow");
-    const navbar = document.getElementById("navbar");
-    navbar.style.position = "fixed";
-    navbar.style.top = "0px";
-    navbar.style.transition = "top 1s";
-  } else {
-    document.getElementById("navbar").classList.remove("navbar-shadow");
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     document.getElementById("navbar").classList.add("navbar-shadow");
+//     const navbar = document.getElementById("navbar");
+//     navbar.style.position = "fixed";
+//     navbar.style.top = "0px";
+//     navbar.style.transition = "top 1s";
+//   } else {
+//     document.getElementById("navbar").classList.remove("navbar-shadow");
+//   }
+// }
 // nav bar fixed
 // back face js
 document.querySelectorAll(".backface-card").forEach((card) => {
@@ -26,3 +26,20 @@ document.querySelectorAll(".backface-card").forEach((card) => {
   });
 });
 // back face js
+// marquee partner 
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+// Clone the original marquee content
+const originalContent = Array.from(marqueeContent.children);
+
+// Reverse the order of elements
+const reversedContent = originalContent.slice().reverse();
+
+// Append clones to the reverse marquee content
+for (let i = 0; i < marqueeElementsDisplayed - 1; i++) { // Adjusted loop to exclude the last item
+  marqueeContent.appendChild(originalContent[i].cloneNode(true));
+}
